14d0f660e826d343f915e71565ba0d0306c1021d
Eldar Gyulmagomedov <steal.vip@gmail.com>
2023-03-03 12:26:30 +0300

    Решил первую задачу. RandomArray. CountEvenNumbers

---
+++ Task001/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+int[] array = CreatArray(15, 1, 999);
+int[] CreatArray(int size, int minValue, int maxValue) // Создание массива
+{
+    int[] result = new int[size]; // Создание массива на size элементов
+    // Если size = 12, то массив состоит из 12 нулей
+    for (int i = 0; i < result.Length; i++) // result.Length = size
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return result; // Вернули массив на size элементов, заполненный числами
+    // от minValue до maxValue
+}
+Console.WriteLine($"Исходный массив: [{String.Join("; ", array)}]");
+int evenNumbers = 0;
+for(int i=0; i< array.Length;i++)
+{
+    if(array[i]%2 == 0)
+    {
+        evenNumbers++;
+    }
+
+}
+System.Console.WriteLine($"Количество четных чисел в массиве = {evenNumbers} ");
